8009f7e54833f9f8275b2ab804f3020d31433d37
Denis <denchiklut@gmail.com>
2018-09-29 19:47:37 -0700

    Сделал роутинг

---
+++ src/components/main.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Landing from "./landingpage";
+import About from "./aboutme";
+import Contact from "./contact";
+import Projects from "./projects";
+import Resume from "./resume";
+import { Switch, Route } from 'react-router-dom'
+
+const Main = () => (
+  <Switch>
+      <Route exact path="/" component={Landing} />
+      <Route path="/about" component={About} />
+      <Route path="/contact" component={Contact} />
+      <Route path="/projects" component={Projects} />
+      <Route path="/resume" component={Resume} />
+  </Switch>
+);
+
+export default Main;
