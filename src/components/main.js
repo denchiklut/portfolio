7ec851d94ff1d0483f197bbd60e81da4d121303c
Denis <denchiklut@gmail.com>
2018-09-30 19:53:06 -0700

    Настроил роутинг

--- src/components/main.js
+++ src/components/main.js
@@ -4,18 +4,16 @@ import About from "./aboutme";
 import Contact from "./contact";
 import Projects from "./projects";
 import Resume from "./resume";
-import {Switch, Route, HashRouter} from 'react-router-dom'
+import {Switch, Route} from 'react-router-dom'
 
 const Main = () => (
-    <HashRouter>
-        <Switch>
-            <Route exact path="/" component={Landing} />
-            <Route path="/about" component={About} />
-            <Route path="/contact" component={Contact} />
-            <Route path="/projects" component={Projects} />
-            <Route path="/resume" component={Resume} />
-        </Switch>
-    </HashRouter>
+    <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/resume" component={Resume} />
+    </Switch>
 );
 
 export default Main;
